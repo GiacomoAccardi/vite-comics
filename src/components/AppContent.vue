b88418cb75eaf4ba8ecfba607c53e7fef65e497b
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-07-29 17:08:51 +0200

    [UPD] AppContent now has styles

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -3,11 +3,20 @@ export default {
     
 }
 </script>
+
 <template lang="">
-    <div>
-        
+    <div class="wrapper bg-dark">
+        <div class="container d-flex align-items-center">
+            <div class="mt-5">
+                <p>-->Content goes here<--</p>
+            </div>
+        </div>
     </div>
 </template>
-<style lang="">
-    
+
+<style scoped>
+    .wrapper {
+        height: 120px;
+        color: white;
+    }
 </style>
