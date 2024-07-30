a4d37ce9227fa6f3b48261966266b6af92681257
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-07-30 17:24:03 +0200

    esercizio completato

--- src/components/productCard.vue
+++ src/components/productCard.vue
@@ -1,16 +1,27 @@
 <script>
 export default {
-    
+    props: {
+        imagePath: String,
+        title: String,
+    }
 }
 </script>
 
 <template lang="">
-    <div class="col-2">
-        <img src="" alt="">
-        <div></div>
+    <div class="col-md-4 col-xl-2">
+        <img :src="imagePath" :alt="title">
+        <h5>{{title}}</h5>
     </div>
 </template>
 
-<style lang="">
-    
+<style lang="scss" scoped>
+    img{
+        max-width: 200px;
+        height: 300px;
+        margin-bottom: 10px;
+        object-fit: fill;
+    }
+    h5{
+        margin-bottom: 30px;
+    }
 </style>
